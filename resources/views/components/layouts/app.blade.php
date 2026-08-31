2b69eb43e225b62ec06f5a01cb266691c6e51306
Chad R Denaux <105078717+FutureLithics@users.noreply.github.com>
2026-08-31 12:05:08 -0400

    feature: add favicon to site (#4)

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ $title ?? config('app.name', 'PVTR License Lookup') }}</title>
+        <link rel="icon" href="{{ asset('favicon.ico') }}">
 
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
